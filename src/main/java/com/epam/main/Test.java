e39b09c06c295a0469b3bdad19dc579b4ef9511f
Igor Hurskyi <Igor_Hurskyi@epam.com>
2018-03-25 00:08:17 +0200

    first commit 12332

--- src/main/java/com/epam/main/Test.java
+++ src/main/java/com/epam/main/Test.java
@@ -8,8 +8,5 @@ public class Test {
         MyArrayList<Integer> mylist = new MyArrayList<>(5);
         mylist.addToStart(6);
         mylist.showElement(0);
-        System.out.println("HELLO");
-        System.out.println("darova");
-        System.out.println("karova");
     }
 }
